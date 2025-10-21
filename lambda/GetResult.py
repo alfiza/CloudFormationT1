89bd55c2b4695fbe2b85cc7e887cab2bd991ea1d
alfiza <alfizamalek@gmail.com>
2025-10-21 09:41:34 +0100

    error code updated .

--- lambda/GetResult.py
+++ lambda/GetResult.py
@@ -5,30 +5,33 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('Results') 
 
 def handler(event, context):
-    
-    params = event.get('queryStringParameters') or {} 
-    name = params.get('name')
+    try:
+        params = event.get('queryStringParameters') or {}
+        name = params.get('name')
 
-    # Validatinng the input data
-    if not name:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({'message': 'Name is missing.'})
-        }
+        # Validatinng the input data
+        if not name:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'message': 'Name is missing.'})
+            }
+            # get data from result table
+            response = table.get_item(Key={'name': name})
+            item = response.get('Item')
 
-    # get data from result table
-    response = table.get_item(Key={'name': name})
-    item = response.get('Item')
+            # If student detail not exsit
+            if not item:
+                return {
+                    'statusCode': 404,
+                    'body': json.dumps({'message': 'Student not found'})
+                }
+            # Return found item
+            return {
+                'statusCode': 200,
+                'body': json.dumps(item)
+            }
 
-    # If student detail not exsit
-    if not item:
-        return {
-            'statusCode': 404,
-            'body': json.dumps({'message': 'Student not found'})
-        }
-
-    # Return found item
-    return {
-        'statusCode': 200,
-        'body': json.dumps(item)
-    }
+    except Exception as e:
+        # error
+        print(f"Error: {str(e)}")
+        return {'statusCode': 500, 'body': json.dumps({'error': str(e)})}
